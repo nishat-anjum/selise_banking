33e915a1234ac9089f771aec588f2556a5f2afdb
nishat-anjum <nanjum.du14@gmail.com>
2024-10-11 18:28:32 +0600

    Initial Commit

---
+++ src/main/java/com/selise/banking/SeliseBankingApplication.java
@@ -0,0 +1,13 @@
+package com.selise.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeliseBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeliseBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selise/banking/SeliseBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selise.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeliseBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
