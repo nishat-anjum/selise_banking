a5c22b18a2f2b60972585f21ab1c19c2e1208e36
nishat-anjum <nanjum.du14@gmail.com>
2024-10-11 21:31:58 +0600

    Added unitTest for bankTransfer

--- src/test/java/com/selise/banking/SeliseBankingApplicationTests.java
+++ src/test/java/com/selise/banking/SeliseBankingApplicationTests.java
@@ -1,13 +1,25 @@
 package com.selise.banking;
 
+import com.selise.banking.model.TransferRequest;
+import com.selise.banking.service.ClientAccountService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
+@Slf4j
 @SpringBootTest
 class SeliseBankingApplicationTests {
 
+	@Autowired
+	private ClientAccountService clientAccountService;
+
 	@Test
 	void contextLoads() {
+		log.debug("##################");
+		TransferRequest transferRequest = new TransferRequest("1234567890", "9876543210", BigDecimal.valueOf(200));
+		clientAccountService.transferAmount(transferRequest);
 	}
-
 }
